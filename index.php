957d16cbc533c58e1bece8b62e388b43f8314eeb
yadiputra <43856886+yadiputra@users.noreply.github.com>
2025-02-18 09:14:20 +0700

    update

---
+++ index.php
@@ -0,0 +1,291 @@
+<!doctype html>
+<html lang="en" data-bs-theme="auto">
+  <head><script src="assets/js/color-modes.js"></script>
+
+    <meta charset="utf-8">
+	<link href="ppds.png" rel="icon">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.118.2">
+    <title>PONDOK PESANTREN DARUSSALAMAH</title>
+    <link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
+	<link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+	<link href="assets/dist/css/all.min.css" rel="stylesheet">
+
+    
+    <!-- Custom styles for this template -->
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"/>
+        
+    <link href="carousel.css" rel="stylesheet">
+    <link href="assets/aos/aos.css" rel="stylesheet">
+  </head>
+  <body>
+<header>
+  <nav class="navbar navbar-expand-md fixed-top bg-darks navbar-light">
+    <div id="logo" class="container-fluid">
+      <a href="https://darussalamah.ponpes.id" class="navbar-brand d-flex align-items-center px-2 px-lg-4 col-md-4 col-9">
+		<div class="col-md-12 col-xs-12 ps-2 ">
+		<img class="img-fluid pt-2 pb-2" src="ponpes.png">
+		</div>
+	  </a>
+	  
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+		<div class="collapse navbar-collapse" id="navbarCollapse">
+		<div class="navbar-nav ms-auto p-4 p-lg-0">
+		<a href="#syarat" class="nav-item nav-link ">Daftar</a>
+		<a href="#syarat" class="nav-item nav-link ">Persyaratan</a>
+		<a href="#agenda" class="nav-item nav-link ">Agenda</a>
+		<a href="#biaya" class="nav-item nav-link ">Biaya</a>
+		<a href="#brosur" class="nav-item nav-link ">Brosur</a>
+		<a href="#kontak" class="nav-item nav-link ">Kontak</a>
+		</div>
+		</div>
+
+    </div>
+  </nav>
+</header>
+
+<main>
+
+<div class="container-fluid bg-ponpes py-4 page-header hide">
+	<div class="container py-5">
+		<div class="row justify-content-center">
+			<div class="col-lg-10 text-center">
+			<div class="carousel-caption-content p-3">
+				<h1 class="display text-capitalize text-white mb-4">PENERIMAAN SANTRI BARU (PSB)</h1>
+				<p class="mb-5 fs-5 text-white">Pendaftaran Santri Baru TP. 2025-2026 Telah Dibuka!! 
+				</p>
+				<a class="btn btn-success bg-ponpes text-white rounded-pill text-white py-3 px-5" href="#">Daftar Sekarang</a>
+			</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+	<div id="syarat" class="container-fluid py-5 wow gray-bg fadeInUp" data-aos="fade-up">
+        <div class="container py-3 px-4">
+            <div class="row g-5">
+                <div class="col-lg-7 mb-3">
+                    <div class="section-title_left position-relative pb-3 mb-2">
+                        <h4 class="fw-bold text-ponpes text-uppercase">Pendaftaran Santri Baru</h4>
+                        <h1 class="mb-0">Pondok Pesantren Darussalamah</h1>
+                    </div>
+                    <p class="mb-4">Pendaftaran Santri Baru Tahun Pelajaran 2025/2026 sudah dibuka Pendaftaran bisa dilakukan secara Offline maupun Online.</p>
+					<p style="text-align: justify;"><strong>Melampirkan :</strong></p>
+                    <div class="row g-0 mb-3">
+                        <div class="col-sm-7 wow zoomIn" data-wow-delay="0.2s" style="visibility: visible; animation-delay: 0.2s; animation-name: zoomIn;">
+                            <h5 class="mb-3"><i class="fa fa-check text-ponpes me-3"></i>Print Out NISN (2 Lembar)</h5>
+                            <h5 class="mb-3"><i class="fa fa-check text-ponpes me-3"></i>Fc Ijazah (5 lembar)</h5>
+							<h5 class="mb-3"><i class="fa fa-check text-ponpes me-3"></i>Fc KTP ayah & Ibu (5 lembar)</h5>
+                            <h5 class="mb-3"><i class="fa fa-check text-ponpes me-3"></i>Fc Akta Kelahiran (5 lembar)</h5>
+							<h5 class="mb-3"><i class="fa fa-check text-ponpes me-3"></i>Materai 10.000 (5 lembar)</h5>
+                           
+                            
+                        </div>
+                        <div class="col-sm-5 wow zoomIn" data-wow-delay="0.4s" style="visibility: visible; animation-delay: 0.4s; animation-name: zoomIn;">
+							<h5 class="mb-3"><i class="fa fa-check text-ponpes me-3"></i>1 Map Gobi</h5>
+                            <h5 class="mb-3"><i class="fa fa-check text-ponpes me-3"></i>Fc Piagam (5 lembar)</h5>
+							<h5 class="mb-3"><i class="fa fa-check text-ponpes me-3"></i>Fc KK (5 lembar)</h5>
+							<h5 class="mb-3"><i class="fa fa-check text-ponpes me-3"></i>Fc PIP/PKH (5 lembar)</h5>
+                         	
+                       
+						</div>
+                    </div>
+                    <a href="#" class="btn btn-success bg-ponpes text-white py-3 px-5 mt-3 wow zoomIn" data-wow-delay="0.9s" style="visibility: visible; animation-delay: 0.9s; animation-name: zoomIn;">Daftar</a>
+                </div>
+                <div class="col-lg-5" style="min-height: 500px;">
+                    <div class="position-relative h-100">
+                        <a href="#"><img class="position-absolute w-100 h-100 rounded wow zoomIn brosur" data-wow-delay="0.9s" src="bosur_.jpg" style="visibility: visible; animation-delay: 0.9s; animation-name: zoomIn;"></a><a>
+                    </a></div><a>
+                </a></div><a>
+            </a></div><a>
+        </a></div><a>
+    </a></div>
+
+<div class="container marketing" id="agenda">
+<div class="container" data-aos="fade-up">
+<div class="row g-5 mb-5">
+<div class="text-center" data-aos="fade-up" data-aos-delay="50">
+<h3 class="section-title bg-white text-center text-merah px-3" style="">Agenda Daftar</h3>
+</div>
+</div>
+</div>
+    <!-- Three columns of text below the carousel -->
+    <!-- /.row -->
+	
+	<div class="container" data-aos="fade-up">
+	 <div class="row row-cols-1 row-cols-sm-2 row-cols-md-2 g-3">
+        <div class="col mbt-2" data-aos="zoom-in" data-aos-delay="100">
+          <div class="card shadow-sm">
+           <div class="card-body text-center">
+			<span class="ms-3 wow fadeInDown fst-italic"><i class="bx bxs-calendar me-1"></i></span>
+              <p class="card-text">04 Februari 2025 s/d 29 Maret 2025</p>
+              <p class="card-text">Gelombang 1</p>
+            </div>
+          </div>
+        </div>
+		<div class="col mbt-2" data-aos="zoom-in" data-aos-delay="100">
+          <div class="card shadow-sm">
+           <div class="card-body text-center">
+			<span class="ms-3 wow fadeInDown fst-italic"><i class="bx bxs-calendar me-1"></i></span>
+              <p class="card-text">07 April 2025 s/d 30 Juli 2025</p>
+              <p class="card-text">Gelombang 2</p>
+            </div>
+          </div>
+        </div>
+        </div>
+        </div>
+        </div>
+		
+		<div id="biaya" class="container-fluid py-2 wow gray-bg fadeInUp" data-wow-delay="0.1s" data-aos="fade-up">
+        <div class="container py-3 px-4">
+            <div class="row g-5">
+                <div class="col-lg-7">
+                    <div class="section-title_left position-relative pb-3 mb-2">
+                        <h4 class="fw-bold text-ponpes text-uppercase">Biaya Pendaftaran</h4>
+                        <h1 class="mb-0">Pondok Pesantren Darussalamah</h1>
+                    </div>
+                    <div class="row g-0 mb-3">
+                        <div class="col-sm-12 wow zoomIn" data-wow-delay="0.2s" data-aos="zoom-in">
+                            <h5 class="mb-3 fw-bold text-ponpes">Biaya Pesantren Santri Baru</h5>
+                            <h5 class="mb-3"><i class="fa fa-check text-ponpes me-3"></i>Pendaftaran Pesantren : Rp. 300.000</h5>
+                            <h5 class="mb-3"><i class="fa fa-check text-ponpes me-3"></i>Infak Pesantren : Rp. 1000.000</h5>
+                            <h5 class="mb-3"><i class="fa fa-check text-ponpes me-3"></i>Seragam Pesantren : Rp. 250.000</h5>
+                            <h5 class="mb-3"><i class="fa fa-check text-ponpes me-3"></i>Seragam Diniyah : Rp. 150.000</h5>
+                        </div>
+                        <div class="col-sm-12 wow zoomIn" data-wow-delay="0.4s" data-aos="zoom-in">
+							<h5 class="mb-3 fw-bold text-ponpes"></i>Biaya Madrasah MTS / MA</h5>
+                            <h5 class="mb-3"><i class="fa fa-check text-ponpes me-3"></i>Infak Madrasah : Rp. 300.000</h5>
+                            <h5 class="mb-3"><i class="fa fa-check text-ponpes me-3"></i>Seragam Madrasah : Rp. 350.000</h5>
+						</div>
+						<div class="col-sm-12 wow zoomIn" data-wow-delay="0.4s" data-aos="zoom-in">
+							<h5 class="mb-3 fw-bold text-ponpes"></i>Biaya Syariah/Bulanan/Juli 2025</h5>
+                            <h5 class="mb-3"><i class="fa fa-check text-ponpes me-3"></i>Bulanan Pendidikan : Rp. 150.000</h5>
+                            <h5 class="mb-3"><i class="fa fa-check text-ponpes me-3"></i>Bulanan Makan : Rp. 400.000</h5>
+						</div>
+                    </div>
+                </div>
+                <div class="col-lg-5" style="min-height: 500px;" data-aos="fade-up">
+                    <div class="position-relative h-100">
+                        <a href="#"><img class="position-absolute w-100 h-100 rounded wow zoomIn brosur" data-wow-delay="0.9s" src="http://localhost/ponpes_mura/assets/img/brosur.PNG" style="visibility: visible; animation-delay: 0.9s; animation-name: zoomIn;"></a><a>
+                    </a></div><a>
+                </a></div><a>
+            </a></div><a>
+        </a></div><a>
+    </a></div>
+	
+<div id="brosur" class="container marketing">
+<div class="container" data-aos="fade-up">
+<div class="row g-5 mb-5">
+<div class="text-center" data-aos="fade-up" data-aos-delay="50">
+<h3 class="section-title bg-white text-center text-merah px-3" style="">Brosur</h3>
+</div>
+</div>
+</div>
+    <!-- Three columns of text below the carousel -->
+    <!-- /.row -->
+	<div class="container" data-aos="fade-up">
+	 <div class="row row-cols-1 row-cols-sm-2 row-cols-md-1 g-3">
+        <div class="col mbt-2" data-aos="zoom-in" data-aos-delay="100">
+          <div class="card shadow-sm">
+           <div class="card-body text-center">
+			<img class="img-flui w-100 h-100 rounded wow zoomIn brosur" data-wow-delay="0.9s" src="brosur1.PNG" style="visibility: visible; animation-delay: 0.9s; animation-name: zoomIn;">
+            </div>
+          </div>
+        </div>
+		<div class="col mbt-2" data-aos="zoom-in" data-aos-delay="100">
+          <div class="card shadow-sm">
+           <div class="card-body text-center">
+			<img class="img-flui w-100 h-100 rounded wow zoomIn brosur" data-wow-delay="0.9s" src="brosur.PNG" style="visibility: visible; animation-delay: 0.9s; animation-name: zoomIn;">
+            </div>
+          </div>
+        </div>
+        </div>
+        </div>
+        </div>
+		
+	<div id="kontak" class="container-fluid py-2 wow gray-bg fadeInUp" data-wow-delay="0.1s" style="visibility: visible; animation-delay: 0.1s; animation-name: fadeInUp;">
+        <div class="container py-2">
+            <div class="row ">
+                <div class="col-lg-6 wow slideInUp" data-wow-delay="0.3s" style="visibility: visible; animation-delay: 0.3s; animation-name: slideInUp;">
+                   <div class="col-lg-12">
+                    <div class="pb-5 d-flex align-items-center wow fadeIn" data-wow-delay="0.1s" style="visibility: visible; animation-delay: 0.1s; animation-name: fadeIn;">
+                        <div class="icn d-flex align-items-center justify-content-center rounded" style="width: 60px; height: 60px;">
+                            <i class="fab fa-whatsapp"></i>
+                        </div>
+                        <div class="ps-4">
+                            <h5 class="">Telepon :</h5>
+                            <a href="https://api.whatsapp.com/send/?phone=6282182700873" target="_blank"><h4 class="text-ponpes mb-0">0821-8270-0873</h4></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-12">
+                    <div class="pb-5 d-flex align-items-center wow fadeIn" data-wow-delay="0.4s" style="visibility: visible; animation-delay: 0.4s; animation-name: fadeIn;">
+                        <div class="icn d-flex align-items-center justify-content-center rounded" style="width: 60px; height: 60px;">
+                            <i class="fa fa-envelope-open"></i>
+                        </div>
+                        <div class="ps-4">
+                            <h5 class="">Email :</h5>
+                            <h4 class="text-ponpes mb-0">admin@darussalamah.ponpes.id</h4>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-12">
+                    <div class="pb-6 d-flex align-items-center wow fadeIn" data-wow-delay="0.8s" style="visibility: visible; animation-delay: 0.8s; animation-name: fadeIn;">
+                        <div class="icn d-flex align-items-center justify-content-center rounded" style="width: 60px; height: 60px;">
+                            <i class="fa fa-map-marker-alt"></i>
+                        </div>
+                        <div class="ps-4">
+                            <h5 class="">Alamat :</h5>
+                            <h4 class="text-ponpes mb-0">Jl. Tapak Libok Desa Marga Sakti <br>Kec. Muara Kelingi Kab. Musi Rawas </h4>
+                        </div>
+                    </div>
+                </div> 
+				<div class="col-lg-12">
+                    <div class="pb-6 d-flex align-items-center wow fadeIn" data-wow-delay="0.8s" style="visibility: visible; animation-delay: 0.8s; animation-name: fadeIn;">
+                        <div class="ps-4">
+						<br><br>
+                         <div class="d-flex align-items-center">
+							<i class="fas fa-share fa-2x text-ponpes me-2"></i>
+							<a class="btn-square btn bg-ponpes text-white rounded-circle mx-1" href="https://web.facebook.com/darussalamah.mr" target="_blank"><i class="fab fa-facebook-f"></i></a>
+							<a class="btn-square btn bg-ponpes text-white rounded-circle mx-1" href="https://www.youtube.com/@darussalamahmusirawas" target="_blank"><i class="fab fa-youtube"></i></a>
+							<a class="btn-square btn bg-ponpes text-white rounded-circle mx-1" href="https://www.instagram.com/darussalamah_musi_rawas" target="_blank"><i class="fab fa-instagram"></i></a>
+							<a class="btn-square btn bg-ponpes text-white rounded-circle mx-1" href="https://www.tiktok.com/@darussalamahmusirawas" target="_blank"><i class="fab fa-tiktok"></i></a>
+						</div>
+						</div>
+                    </div>
+                </div>
+		
+                </div>
+                <div class="col-lg-5 wow slideInUp" data-wow-delay="0.6s" style="visibility: visible; animation-delay: 0.6s; animation-name: slideInUp;">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3984.495790237439!2d103.22151667497035!3d-2.959841297016337!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e30614e0d7913d3%3A0xa9ba5c4d826206f6!2sPondok%20Pesantren%20Darussalamah%20Musi%20Rawas!5e0!3m2!1sid!2sid!4v1739428244177!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></div>
+            </div>
+        </div>
+    </div>	
+	  </div>
+  </div><!-- /.container -->
+  <!-- FOOTER -->
+  <footer class="container-fluid bg-white" id="footer">
+  
+    <div class="container footer-bottom clearfix">
+      <div class="copyright">
+        © Copyright <script></script>2025 - <a href="#" title="">Pondok Pesantren Darussalamah</a>
+      </div>  
+    </div>
+  </footer>
+</main>
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  
+  <script src="assets/dist/js/bootstrap.bundle.min.js"></script>
+
+
+  <script src="assets/js/main.js"></script>
+
+    </body>
+</html>
